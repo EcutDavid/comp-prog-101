dd52ad75d615728bc55e069b2893ccf79878c0fe
David Guan <davidguandev@gmail.com>
2019-02-22 21:14:49 +1100

    some slides

---
+++ demo/scan/solution.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+	// a := make([]int, n) // make a slice that contains n elements
+	// for i := 0; i < n; i++ {
+	// 	fmt.Scan(&a[i])
+	// }
+	// 5 * 10^5 numbers
+	// numTotal := 5e5
+	// for i := 0; i < int(numTotal); i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// a := 0
+	// for i := 0; i < int(numTotal); i++ {
+	// 	fmt.Scan(&a)
+	// 	fmt.Println(a)
+	// }
+
+	// a := 0
+	// scanner := bufio.NewScanner(os.Stdin)
+	// for i := 0; i < int(numTotal); i++ {
+	// 	scanner.Scan()
+	// 	a, _ = strconv.Atoi(scanner.Text())
+	// 	fmt.Println(a)
+	// }
+
+	// a := 0
+	// writer := bufio.NewWriter(os.Stdout)
+	// scanner := bufio.NewScanner(os.Stdin)
+	// for i := 0; i < int(numTotal); i++ {
+	// 	scanner.Scan()
+	// 	a, _ = strconv.Atoi(scanner.Text())
+	// 	writer.WriteString(strconv.Itoa(a) + "\n")
+	// }
+	// writer.Flush()
+}
